8ef7ecc6cee9c74d14912a9201855a1fff7d1f45
johnwsh <johnwillian0609@gmail.com>
2025-06-28 11:22:34 -0300

    coisas

---
+++ criptAndBinary.py
@@ -0,0 +1,49 @@
+import base64
+import os
+from cryptography.fernet import Fernet
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+
+
+
+def encryptMessage(message: bytes, password: bytes): 
+    salt = b'\x87z\xe4\xa93{\t\x8f#xF,yy\xc2\x05'
+
+    kdf = PBKDF2HMAC(
+        algorithm=hashes.SHA256(),
+        length=32,
+        salt=salt,
+        iterations=1_200_000,
+    )
+
+    key = base64.urlsafe_b64encode(kdf.derive(password))
+
+    f = Fernet(key)
+
+    token = f.encrypt(message)
+    return token
+
+def decryptMessage(token: bytes, password: bytes):
+    salt = b'\x87z\xe4\xa93{\t\x8f#xF,yy\xc2\x05'
+
+    kdf = PBKDF2HMAC(
+        algorithm=hashes.SHA256(),
+        length=32,
+        salt=salt,
+        iterations=1_200_000,
+    )
+
+    key = base64.urlsafe_b64encode(kdf.derive(password))
+
+    f = Fernet(key)
+
+    message = f.decrypt(token)
+    return message
+
+def binarize(data: bytes):
+    return ''.join(format(byte, '08b') for byte in data)
+
+def debinarize(data: str):
+    bytes_list = [data[i:i + 8] for i in range(0, len(data), 8)]
+    return bytes(int(byte, 2) for byte in bytes_list)
+
